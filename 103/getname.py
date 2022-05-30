fcc805b5733470350536fbf82a5160a20a73eb22
honeming <57797481+honeming@users.noreply.github.com>
2022-05-30 19:40:02 +0800

    removed

--- 103/getname.py
+++
@@ -1,9 +0,0 @@
-import random
-f=open('names.txt','r')
-a=f.read().splitlines()
-f.close()
-n=a.pop(random.randint(0,len(a)-1))
-f=open('names.txt','w')
-f.write("\n".join(a))
-f.close()
-input("name:"+n+'\nleft:'+str(len(a)))
